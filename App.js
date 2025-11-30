e5929a61eaa9946907c058e8728dfc49bf34667d
anaselhoumat21-sys <anaselhoumat21@gmail.com>
2025-11-30 23:13:07 +0100

    Fix formatting and add placeholder text in App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./component/Home";
 import ProductDetail from "./component/ProductDetail";
@@ -13,6 +13,7 @@ class App extends Component {
           <Route
             path="*"
             element={<h2 className="text-center mt-5">Page non trouvée</h2>}
+              <p>ksdbcghvjhb </p>
           />
         </Routes>
       </Router>
